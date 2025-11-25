bfbf3c5f110f3eac3a5edbcbbe1e28c607271809
adryeldeev <adryeldev@hotmail.com>
2025-11-25 20:18:32 -0300

    Classe criadas Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1.getTitulo());
+        System.out.println(curso1.getDescricao());
+        System.out.println(curso1.getCargaHoraria());
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        System.out.println(
-                String.format("This is line %d", i)
+        System.out.println("==============================");
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição do curso JavaScript");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso2.getTitulo());
+        System.out.println(curso2.getDescricao());
+        System.out.println(curso2.getCargaHoraria());
 
-        );
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição da mentoria Java");
+        mentoria.setData(java.time.LocalDate.now());
+        System.out.println(mentoria.getTitulo());
+        System.out.println(mentoria.getDescricao());
+        System.out.println(mentoria.getData());
+
+
     }
 }
